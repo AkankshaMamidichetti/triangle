ef139587f72a2db4d09b41238c698265be895723
AkankshaMamidichetti <makanksha6497@gmail.com>
2025-08-06 18:36:47 +0530

    Add files via upload

---
+++ 06-08-25.py
@@ -0,0 +1,31 @@
+# write a program to check if three sides length form a valid and type of triangle
+# triangle. 
+# def vaild_triangle(side1,side2,side3):
+#     if (side1+side2 > side3) and (side1+side3 > side2) and (side2+side3>side1):
+#         print("it form a triangle")
+#         if side1==side2==side3:
+#             print("form equalatral triangle")
+#         elif side1==side2!=side3 or side3==side2!=side1 or side1==side3!=side2:
+#             print("forms iso")
+#         elif side1!=side2!=side3:
+#             print("scale-in triangle")
+#     else:
+#         print("it doesn't form a triangle")
+# side1=int(input("enter first side:"))
+# side2=int(input("enter second side:"))
+# side3=int(input("enter third side:"))
+# vaild_triangle(side1,side2,side3)
+# write a program to check if three sides length form a valid and pythogorus(right angled)
+# def vaild_triangle(side1,side2,side3):
+#     if (side1+side2 > side3) and (side1+side3 > side2) and (side2+side3>side1):
+#         print("it form a triangle")
+#         if ((side1)**2)+((side2)**2)==(side3**2) or ((side2)**2)+((side3)**2)==(side1**2) or ((side3)**2)+((side1)**2)==(side2**2):
+#             print("it follows pythogoras theorem")
+#         else:
+#             print("not pythogoras")
+#     else:
+#         print("it doesn't form a triangle")
+# side1=int(input("enter first side:"))
+# side2=int(input("enter second side:"))
+# side3=int(input("enter third side:"))
+# vaild_triangle(side1,side2,side3)
